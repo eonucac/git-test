8c72d29910baf9afbe4774e54d6f4db7fc7c3389
eonucac <2483917248@qq.com>
2023-07-24 01:40:19 +0800

    test commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,7 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git!");
-        System.out.println("hello,git3!");
-        System.out.println("hello,git4!");
+        System.out.println("hello,git2!");
     }
 }
